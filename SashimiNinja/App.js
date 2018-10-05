0a5e02b592bc9a4184ee0db484e92ce5bf2ce8e1
Jessica Karpovich <karpovichjm@gmail.com>
2018-10-04 22:12:26 -0700

    Added logic to test getting Hiragana from db.

--- SashimiNinja/App.js
+++ SashimiNinja/App.js
@@ -2,18 +2,58 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import * as firebase from 'firebase';
 import 'firebase/firestore';
+import { Button } from 'react-native';
+import {Key, Domain, ID} from 'react-native-dotenv';
 
 export default class App extends React.Component {
 
-  
+  // to test db connection and retrieval
+  constructor(props) {
+    super(props);
+
+    // set state, hiragana is array of objects
+    this.state = {
+      hiragana: []
+    }
+    // bind function with retrieval logic
+    this.getHiragana = this.getHiragana.bind(this);
+
+  }
+
+  componentDidMount() {
+    // get hiragana right away, later add code to save it to asyncstorage
+    this.getHiragana();
+  }
+
+  // function to pull in hiragana from db
+  getHiragana() {
+    // specify collection and doc
+    let hiraganaDoc = db.collection("hiragana").doc("bC7F87A7wWhMAoxgpBhK");
+    let hArray;
+
+    // use arrow function to have correct "this"
+    hiraganaDoc.get().then((doc) => {
+      // check for data, if exists, save it to state
+      if (doc.exists) {
+          hArray = doc.data().array;
+          console.log(hArray);
+          this.setState({hiragana: hArray});
+      } else {
+          // doc.data() will be undefined in this case
+          console.log("No such document!");
+      }// catch errors
+    }).catch(function(error) {
+        console.log("Error getting document:", error);
+    });
+  }
 
   render() {
-    let hiragana = db.collection("hiragana").doc("1XVxg9OHvYcNAlf8kZKx");
-    //<Text>{ hiragana }</Text>
-
+    // in render, display kana
     return (
       <View style={styles.container}>
         <Text>Open up App.js to start working on your app!</Text>
+        <Button onPress={this.getHiragana} title="Get hiragana"></Button>
+        <Text>{ JSON.stringify(this.state.hiragana) }</Text>
       </View>
     );
   }
@@ -28,11 +68,11 @@ const styles = StyleSheet.create({
   },
 });
 
-// init firebase
+// init firebase, use env vars
 const fbConfig = {
-  apiKey: "AIzaSyCgjYfnJW54uqwL0FyGlqchLrJnl63uJVs",
-  authDomain: "sashimininja36.firebaseapp.com",
-  projectId: "sashimininja36"
+  apiKey: Key,
+  authDomain: Domain,
+  projectId: ID
 };
 
 // Initialize Cloud Firestore through Firebase
