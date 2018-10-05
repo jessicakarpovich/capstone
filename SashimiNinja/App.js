99f87f4f4928d85cdafd165fa4dfaa370fedc3c6
Jessica Karpovich <karpovichjm@gmail.com>
2018-10-04 19:37:23 -0700

    Added code to connect to db.

--- SashimiNinja/App.js
+++ SashimiNinja/App.js
@@ -1,8 +1,16 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import * as firebase from 'firebase';
+import 'firebase/firestore';
 
 export default class App extends React.Component {
+
+  
+
   render() {
+    let hiragana = db.collection("hiragana").doc("1XVxg9OHvYcNAlf8kZKx");
+    //<Text>{ hiragana }</Text>
+
     return (
       <View style={styles.container}>
         <Text>Open up App.js to start working on your app!</Text>
@@ -18,4 +26,20 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+});
+
+// init firebase
+const fbConfig = {
+  apiKey: "AIzaSyCgjYfnJW54uqwL0FyGlqchLrJnl63uJVs",
+  authDomain: "sashimininja36.firebaseapp.com",
+  projectId: "sashimininja36"
+};
+
+// Initialize Cloud Firestore through Firebase
+firebase.initializeApp(fbConfig);
+const db = firebase.firestore();
+
+// Disable deprecated features
+db.settings({
+  timestampsInSnapshots: true
 });
