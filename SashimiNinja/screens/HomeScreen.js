79137254d4a225373da7f2d22ef9876386f8d586
Jessica Karpovich <karpovichjm@gmail.com>
2018-10-14 16:55:58 -0500

    Completed logic to fetch kanji/phrase of the day.

--- SashimiNinja/screens/HomeScreen.js
+++ SashimiNinja/screens/HomeScreen.js
@@ -4,6 +4,7 @@ import { StyleSheet, Text, View, AsyncStorage } from 'react-native';
 import Colors from '../constants/Colors';
 import LogoIcon from '../constants/LogoIcon';
 import { MonoText } from '../components/StyledText';
+import { app } from 'firebase';
 
 export default class HomeScreen extends React.Component {
 
@@ -12,12 +13,13 @@ export default class HomeScreen extends React.Component {
   // kIndex and pIndex are calculated using the dayIndex
   // to display a different kanji/phrase based on length of each array
   state = {
-    kanjiArray: [],
-    phrasesArray: [],
-    date: 0,
-    dayIndex: 0,
-    kIndex: 0,
-    pIndex: 0,
+    kanjiArray: null,
+    phrasesArray: null,
+    date: null,
+    dayIndex: null,
+    kIndex: null,
+    pIndex: null,
+    loaded: false,
   }
 
   // for now hardcode it to display home, 
@@ -34,9 +36,15 @@ export default class HomeScreen extends React.Component {
   };
 
   // get data from AsyncStorage 
-  componentDidMount() {
+  async componentDidMount() {
+    await this.getData();
+    this.calcKanjiPhrase();
+  }
+
+  getData = async () => {
+    let temp;
     // get kanji array from AsyncStorage
-    AsyncStorage.getItem('kanji')
+    /*AsyncStorage.getItem('kanji')
       .then((kanjiArray) => {
         // check for valid data
         if (kanjiArray) {
@@ -48,135 +56,164 @@ export default class HomeScreen extends React.Component {
             console.log("Error in retrieving character array. " + e.message);
           }
         }
-      })
-    // get phrases array from AsyncStorage
-    AsyncStorage.getItem('phrases')
-      .then((phrasesArray) => {
-        // check for valid data
-        if (phrasesArray) {
-          try {
-            // parse it to JSON
-            let temp = JSON.parse(phrasesArray);
-            this.setState({phrasesArray: temp});
-          } catch(e) {
-            console.log("Error in retrieving array. " + e.message);
-          }
-        }
-      })
+      })*/
+    try {
+      temp = await AsyncStorage.getItem('kanji');
+      if (temp !== null) {
+        this.setState({kanjiArray: JSON.parse(temp)});
+      }
+    } catch (err) {
+      console.log("Error getting kanji array", err);
+    }
+    try {
+      temp = await AsyncStorage.getItem('phrases');
+      if (temp !== null) {
+        this.setState({phrasesArray: JSON.parse(temp)});
+      }
+    } catch (err) {
+      console.log("Error getting phrases array", err);
+    }
+    try {
+      temp = await AsyncStorage.getItem('date');
+      if (temp !== null) {
+        this.setState({date: JSON.parse(temp)});
+      }
+    } catch (err) {
+      console.log("Error getting date", err);
+    }
     // get date from AsyncStorage
-    AsyncStorage.getItem('date', (err, date) => {
+    /*AsyncStorage.getItem('date', (err, date) => {
       // check for valid data
       if (date !== null) {
         // parse it to JSON
-        let temp = JSON.parse(date);
-        this.setState({date: temp});
+        let appDate = JSON.parse(date);
+        this.setState({date: appDate});
       } if (err) {
         console.log(err); 
       }
-    })
+    })*/
+    try {
+      temp = await AsyncStorage.getItem('dayIndex');
+      if (temp !== null) {
+        console.log(temp);
+        this.setState({dayIndex: JSON.parse(temp)});
+      }
+    } catch (err) {
+      console.log("Error getting day index", err);
+    }
     // get dayIndex from AsyncStorage
-    AsyncStorage.getItem('dayIndex')
+    /*AsyncStorage.getItem('dayIndex')
       .then((index) => {
         // check for valid data
         if (index) {
           try {
-            this.setState({dayIndex: JSON.parse(index)});
+            dayIndex = JSON.parse(index);
+            this.setState({dayIndex: dayIndex});
           } catch(e) {
             console.log("Error in retrieving date. " + e.message);
           }
         }
-      })
+      })*/
+    
 
-    // calc dayIndex, kIndex, and pIndex
-    this.calcKanjiPhrase();
+    //this.setState({loaded: true});
   }
 
-  calcKanjiPhrase = async () => {
+  calcKanjiPhrase = () => {
     // get current date
     let temp = new Date();
     let now = temp.getDay();
+    let date = this.state.date;
     let index = this.state.dayIndex;
+    let kLength = null;
+    let pLength = null;
 
     // if today is a different day from stored day, update it 
-    if (now != this.state.date) {
+    if (now != date) {
+      console.log(now);
+      console.log(date);
       // if date has changed, update values used to choose kanji and phrase
       console.log("Not");
-
       // TO-DO: complete the logic to update values
       // test with different dates to make sure it works
-      index++;
+      index = index+1;
       this.setState({dayIndex: index});
+      this.setState({date: date});
 
+      AsyncStorage.setItem('date', JSON.stringify(now));
+      AsyncStorage.setItem('dayIndex', JSON.stringify(index));
     }
-    let kLength = this.state.kanjiArray.length;
-    let kanjiIndex = 0;
-    let pLength = this.state.phrasesArray.length;
-    let pIndex = 0;
+    if (this.state.kanjiArray) {
+      kLength = this.state.kanjiArray.length;
+      console.log("sadsa");
+      console.log(kLength);
+    }
+    let kanjiIndex = index;
+    if (this.state.phrasesArray) {
+      pLength = this.state.phrasesArray.length;
+    }
+    let pIndex = index;
 
     // logic to choose which kanji and phrase to show
     if (index < kLength) {
       kanjiIndex = index;
-    } else {
+    } else if (kLength > 0) {
       kanjiIndex = index % kLength;
+      console.log("daw");
+      console.log(index);
+      console.log(kLength);
+      console.log(kanjiIndex);
     }
     if (index < pLength) {
       pIndex = index;
-    } else {
+    } else if (pLength > 0) {
       pIndex = index % pLength;
     }
     this.setState({kIndex: kanjiIndex});
     this.setState({pIndex: pIndex});
+
+    this.setState({loaded: true});
   }
 
   render() {
-    // later look into simplifying this
-    // currently keeping to avoid undefined errors on render
-    let kanjiArray = [{}];
-    let phrasesArray = [{}];
-    let kIndex = 0;
-    let pIndex = 0;
-    if (this.state.kanjiArray.length > 0) {
-      kanjiArray = this.state.kanjiArray;
-    }
-    if (this.state.kIndex) {
-      kIndex = this.state.kIndex;
-    }
-    if (this.state.phrasesArray.length > 0) {
-      phrasesArray = this.state.phrasesArray;
-    }
-    if (this.state.pIndex) {
-      pIndex = this.state.pIndex;
+    //if (!this.state.kanjiArray || !this.state.kIndex) {
+    if (!this.state.loaded) {
+      //console.log(this.state.kanjiArray);
+      //console.log(this.state.kIndex);
+      return (
+        <Text>Hello</Text>
+      );
     }
 
     return (
       <View style={styles.container}>
         <View style={styles.contentCont}>
           <Text style={styles.title}>Kanji of the Day</Text>
-          <Text style={styles.character}>{kanjiArray[kIndex].kanji}</Text>
+          <Text style={styles.character}>{this.state.kanjiArray[this.state.kIndex].kanji}</Text>
           <View style={styles.rowCenter}>
             <Text style={styles.boldLabelText}>Kunyomi: </Text>
-            <Text style={styles.labelText}>{ kanjiArray[kIndex].kunyomi }</Text>
+            <Text style={styles.labelText}>{ this.state.kanjiArray[this.state.kIndex].kunyomi }</Text>
           </View>
           <View style={styles.rowCenter}>
             <Text style={styles.boldLabelText}>Onyomi: </Text>
-            <Text style={styles.labelText}>{ kanjiArray[kIndex].onyomi }</Text>
+            <Text style={styles.labelText}>{ this.state.kanjiArray[this.state.kIndex].onyomi }</Text>
           </View>
           <View style={styles.rowCenter}>
             <Text style={styles.boldLabelText}>Meaning: </Text>
-            <Text style={styles.labelText}>{ kanjiArray[kIndex].meaning }</Text>
+            <Text style={styles.labelText}>{ this.state.kanjiArray[this.state.kIndex].meaning }</Text>
           </View>
         </View>
 
         <View style={styles.contentCont}>
           <Text style={styles.title}>Phrase of the Day</Text>
-          <Text style={styles.phrase}>{ phrasesArray[pIndex].hiragana }</Text>
+          <Text style={styles.phrase}>{ this.state.phrasesArray[this.state.pIndex].hiragana }</Text>
           <View style={styles.rowCenter}>
             <Text style={styles.boldLabelText}>Romaji: </Text>
-            <Text style={styles.labelText}>{ phrasesArray[pIndex].romaji }</Text>
+            <Text style={styles.labelText}>{ this.state.phrasesArray[this.state.pIndex].romaji }</Text>
           </View>
           <View style={styles.rowCenter}>
             <Text style={styles.boldLabelText}>Meaning: </Text>
-            <Text style={styles.labelText}>{ phrasesArray[pIndex].meaning }</Text>
+            <Text style={styles.labelText}>{ this.state.phrasesArray[this.state.pIndex].meaning }</Text>
           </View>
         </View>
       </View>
